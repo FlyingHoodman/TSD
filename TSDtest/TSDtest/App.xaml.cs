4e1477b0ecbe41e245d11c26f4d1a9d466868afb
Solus Regnator <legendwyvernkiller@googlemail.com>
2017-09-02 17:55:06 +0200

    FIRST initial commit

---
+++ TSDtest/TSDtest/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TSDtest
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new TSDtest.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
